73135f66ea8e8a48085b2e443bc5b33b32ae8664
vuhoaian <vuhoaian1804@gmail.com>
2023-11-18 15:43:43 +0700

    day6

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
 const Product = new Schema({
     name: { type: String, maxLength: 255, require: true, },
+    slug: { type: String, slug: 'name', unique: true },
     description: { type: String, maxLength: 600 },
     content: { type: String},
     img: { type: String, maxLength: 255 },
     price: { type: Number},
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
-});
+    
+},
+{ timestamps: true }
+);
 
 module.exports = mongoose.model('Product', Product);
